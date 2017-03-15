8abe6294d6eaf80c1b849a71e70758ee8749842d
You-Sheng Yang <vicamo@gmail.com>
2017-03-15 13:57:33 +0800

    libpackagelistparser: fix file location

--- libpackagelistparser/Android.mk
+++ libpackagelistparser/Android.mk
@@ -4,9 +4,11 @@ lib_LTLIBRARIES += \
 %canon_reldir%_libandroid_packagelistparser_la_CPPFLAGS = \
     $(AM_CPPFLAGS) \
     $(BIONIC_CFLAGS) \
-    -I$(srcdir)/%reldir%/include
+    -I%reldir%/include
 %canon_reldir%_libandroid_packagelistparser_la_SOURCES = \
     %reldir%/packagelistparser.c
+nodist_%canon_reldir%_libandroid_packagelistparser_la_SOURCES = \
+    %reldir%/include/packagelistparser/packagelistparser.h
 %canon_reldir%_libandroid_packagelistparser_la_LDFLAGS = \
     $(AM_LDFLAGS) \
     $(libtool_opts)
@@ -14,8 +16,11 @@ lib_LTLIBRARIES += \
     liblog/libandroid-log.la
 
 %canon_reldir%_libandroid_packagelistparser_incdir = $(androidincdir)/packagelistparser
-%canon_reldir%_libandroid_packagelistparser_inc_HEADERS = \
+nodist_%canon_reldir%_libandroid_packagelistparser_inc_HEADERS = \
     %reldir%/include/packagelistparser/packagelistparser.h
+
+EXTRA_DIST += \
+    %reldir%/include/packagelistparser/packagelistparser.h.in
 
 pkgconfig_DATA += \
     %reldir%/android-packagelistparser-$(LAARID_API_VERSION).pc
